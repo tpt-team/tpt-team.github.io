b0dfc6d5138f4d092e3b498e315fc8fe89dfd161
AndriyPedan <andriypedan.p@gmail.com>
2016-05-14 14:45:51 +0300

    Add asociations between User and Uni

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,3 +1,4 @@
 class University < ActiveRecord::Base
   has_many :faculties
+  has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514104526) do
+ActiveRecord::Schema.define(version: 20160514114011) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160514104526) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "university_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
