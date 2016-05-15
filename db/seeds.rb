c0d24f00beb23655efb0c02b47809ec9e1d188a2
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-05-15 03:50:57 +0300

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 universities = [
   'Черкасский институт банковского дела',
   'Восточноевропейский университет экономики и менеджмента (ВУЭМ)',
