09b23c70d94f738c7efa0fb3315a44a9b645a4b5
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-05-15 10:34:27 +0300

    add count downloads

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,9 +2,15 @@ class Document < ActiveRecord::Base
   belongs_to :category
   belongs_to :faculty
   belongs_to :user
-  
+
   has_attached_file :document, :path => ':rails_root/public/system/documents/:attachment/:id/:style/:filename',
     :url => '/system/documents/:attachment/:id/:style/:filename'
   validates_attachment_presence :document
   do_not_validate_attachment_file_type :document
+
+  def increment(by = 1)
+    self.downloads ||= 0
+    self.downloads += by
+    self.save
+  end
 end
